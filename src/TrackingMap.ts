5b1fd6b96b934438b51c9d44fb9f7aa9eb1267c5
jenslottosen <jenslottosen@gmail.com>
2022-10-22 18:45:32 +0200

    Displaying map and random markers

---
+++ src/TrackingMap.ts
@@ -0,0 +1,30 @@
+interface Entity {
+  location: {
+    lat: number;
+    lon: number;
+  };
+}
+
+export class TrackingMap {
+  private googleMap: google.maps.Map;
+
+  constructor(elementId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(elementId), {
+      zoom: 1.6,
+      center: {
+        lat: 0,
+        lng: 20,
+      },
+    });
+  }
+
+  attachMarker(entity: Entity): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: entity.location.lat,
+        lng: entity.location.lon,
+      },
+    });
+  }
+}
